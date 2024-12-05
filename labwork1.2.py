4f4e9121514eb7ae4edc27a94b36952b9022838a
DevSurana-07 <devsurana07@gmail.com>
2024-12-05 13:28:04 +0530

    Lab work of introduction

---
+++ labwork1.2.py
@@ -0,0 +1,77 @@
+# Lab Work 1.2
+
+#(Q1)
+
+print("DEV","DHRUV","HARSH","MAYANK", sep=" & ", end = " ! ")
+print("a","b","c","d", sep = " , " , end = "_______")
+
+print("second answer")
+
+#(Q2)
+
+name = input("enter name: ")
+age = input("enter age: ")
+hobby = input("enter hobby: ")
+print("Hello ," +name+ " At " +age+ ", enjoying " +hobby+ " sounds fun! ")
+
+#Q3
+
+a= int(input("ENTER THE NUMBER: "))
+b= int(input("ENTER THE NUMBER: "))
+print(a+b,a-b,a/b,a//b,a*b,a**b,a%b)
+
+#Q4
+
+a = 1
+b = 20.3
+c = "dev"
+d = True
+list1 = [1,2,3]
+tuple1 = ("dev", "dhruv", "game")
+dict1 =  {1: 'pro', 2: 'is', 3: 'pro'}
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(list1))
+print(type(tuple1))
+print(type(dict1))
+
+
+#Q5
+
+weight = float(input("ENTER THE WEIGHT : "))
+height = float(input("ENTER THE HEIGHT : "))
+print(height,weight)
+
+#Q6
+a=input("enter the boolean : ")
+b=input("enter the boolean : ")
+print(a and b)
+print(a or b)
+print(not a)
+print(not b)
+
+
+
+
+
+#Q7
+
+a=20
+print("The value of a is: ", a)
+a+=1
+print("The value of a+=1 is: ", a)
+a=20
+a-=2
+print("The value of a-=2 is: ", a)
+a=20
+a*=3
+print("The value of a*=3 is: ", a)
+a=20
+a/=4
+print("The value of a/=4 is: ", a)
+
+
+
+
